21133e263d7d87039625f68188632e733514ca63
DenWinter <dwinter@mail.ru>
2017-05-15 19:35:26 +0300

    Старт

---
+++ src/ru/dwinter/controllers/Controller.java
@@ -0,0 +1,4 @@
+package ru.dwinter.controllers;
+
+public class Controller {
+}
---
+++ src/ru/dwinter/gui/Map.java
@@ -0,0 +1,12 @@
+package ru.dwinter.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by dwinter on 15.05.17.
+ */
+public class Map extends JTable {
+    private int mapSize = 400;
+    private int cellSize = 5;
+
+}
